31d051d4e3f5c5e233b637cdefcf1d7cfd38f938
Vinicius Libano <34941776+ViniciusLibano@users.noreply.github.com>
2024-09-02 11:08:05 -0300

    gerar atributos tormenta20 ou dnd

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def root():
     return {"Use":"/XdY para rolar dados."}
 
 @app.get("/{x}d{y}")
-def rolarDados(x:int, y:int):
-
+def rolar_dados(x:int, y:int):
     resultados = []
     for i in range(x):
         resultados.append(random.randrange(0,y)+1)
@@ -19,3 +18,23 @@ def rolarDados(x:int, y:int):
         "Lados":y,
         "Resultados":resultados
     }
+
+@app.get("/gerarAtributos")
+def gerar_atributos():
+    atributos = []
+    removidos = []
+    for i in range(6):
+        tempList = []
+        for j in range(4):
+            tempList.append(random.randrange(0,6)+1)
+        
+        tempList.sort()
+        tempList.pop(0)
+        
+        tempVar = 0
+        for dado in tempList:
+            tempVar+=dado
+
+        atributos.append(tempVar)
+
+    return {"Atributos":atributos}
